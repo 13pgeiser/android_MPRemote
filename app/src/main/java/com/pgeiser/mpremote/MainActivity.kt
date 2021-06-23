6631269b03faac58b2a6c0769c425b13ab4ab9f2
Pascal Geiser <pgeiser@pgeiser.com>
2021-06-23 10:53:35 +0200

    Ask for permissions

--- app/src/main/java/com/pgeiser/mpremote/MainActivity.kt
+++ app/src/main/java/com/pgeiser/mpremote/MainActivity.kt
@@ -1,13 +1,63 @@
 package com.pgeiser.mpremote
 
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.annotations.AfterPermissionGranted
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val REQUEST_CODE_PERMISSION = 42 // No comment...
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timber.i("onCreate!")
+        methodRequirePermissions()
+    }
+
+    // Ctrl-o (override members) -> onRequestP...
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // EasyPermissions handles the request result.
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    @AfterPermissionGranted(REQUEST_CODE_PERMISSION)
+    private fun methodRequirePermissions() {
+        // Default permissions needed
+        var permissionList = arrayListOf(
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.BLUETOOTH_ADMIN,
+        )
+        // Starting with Android 6: Coarse location needed
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            permissionList.add(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+        // Starting with Android 10: Fine location needed
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+        val permissions = permissionList.toTypedArray()
+
+        if (EasyPermissions.hasPermissions(this, *permissions)) {
+            // Already have permission, do the thing
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(
+                this,
+                "I need bluetooth permissions...",
+                REQUEST_CODE_PERMISSION,
+                *permissions
+            )
+        }
     }
 }
